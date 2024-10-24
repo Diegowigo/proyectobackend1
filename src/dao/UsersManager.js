5d609f63e3dd5f3bade0cdd6212cd0cfed8bee49
diwigo <dwigodski@gmail.com>
2024-10-24 12:29:40 -0300

    add crud users

--- src/dao/UsersManager.js
+++ src/dao/UsersManager.js
@@ -5,8 +5,22 @@ export class UsersManager {
     return await usersModel.findOne(filter).lean();
   }
 
+  static async getAll(filter = {}) {
+    return await usersModel.find(filter).lean();
+  }
+
   static async addUser(user = {}) {
     let newUser = await usersModel.create(user);
     return newUser.toJSON();
+  }
+  static async updateUser(id, updatedData = {}) {
+    return await usersModel.findByIdAndUpdate(id, updatedData, {
+      new: true,
+      lean: true,
+    });
+  }
+
+  static async deleteUser(id) {
+    return await usersModel.findByIdAndDelete(id);
   }
 }
